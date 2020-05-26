57bb8b5c7ccea78a9202508ed78011ccde5cd70b
cnv_jpalencia <jpalencia@coopnuevavida.com>
2020-05-25 20:44:18 -0600

    MDI

--- Aplicacion1/Aplicacion1/Controllers/ProductController.cs
+++ Aplicacion1/Aplicacion1/Controllers/ProductController.cs
@@ -2,7 +2,9 @@
 using Aplicacion1.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.ModelBinding;
 using System.Web.Mvc;
@@ -22,11 +24,30 @@ namespace Aplicacion1.Controllers
             return View(products);
         }
 
+        //GET: Product/Details/5
+        public ActionResult Details(int? id) {
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var producto = db.Products.Find(id);
+
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(producto);
+
+        }
+
         // GET: Product/Create
         public ActionResult Create() {
 
             return View();
-        
+
         }
 
         // POST: Product/Create
@@ -45,6 +66,47 @@ namespace Aplicacion1.Controllers
             }
             catch (Exception)
             {
+                return View(product);
+            }
+
+        }
+
+        //GET: Product/Edit/5
+        public ActionResult Edit(int? id) {
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var producto = db.Products.Find(id);
+
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(producto);
+
+        }
+
+        //POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Product product) {
+
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(product).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(product);
+            }
+            catch (Exception)
+            {
+
                 return View(product);
             }
 
