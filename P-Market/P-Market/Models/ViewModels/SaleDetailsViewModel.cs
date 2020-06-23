8f18e67f31a612228836ad8694bf6468f599100f
cnv_jpalencia <jpalencia@coopnuevavida.com>
2020-06-22 22:22:54 -0600

    MasterDetails

---
+++ P-Market/P-Market/Models/ViewModels/SaleDetailsViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace P_Market.Models.ViewModels
+{
+    public class SaleDetailsViewModel
+    {
+        [Display(Name = "Codigo")]
+        public int ProductId { get; set; }
+        
+        [Display(Name = "Nombre")]
+        public String ProductName { get; set; }
+
+        [Display(Name = "Precio")]
+        public decimal ProductPrice { get; set; }
+
+        [Display(Name = "Cantidad")]
+        public int ProductQuantity { get; set; }
+
+        [Display(Name = "Sub Total")]
+        public decimal SaleDetailsTotal {
+            get {
+                return ProductPrice * ProductQuantity;
+            }
+        }
+
+    }
+}
---
+++ P-Market/P-Market/Models/ViewModels/SaleViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace P_Market.Models.ViewModels
+{
+    public class SaleViewModel
+    {
+        [Display(Name = "Codigo")]
+        public int ClientKey { get; set; }
+
+        [Display(Name = "Cliente")]
+        public string ClientName { get; set; }
+
+        [Display(Name = "Total")]
+        public decimal SaleTotal {
+            get {
+                decimal tot = 0;
+
+                if (SaleDetails != null)
+                {
+                    foreach (var producto in SaleDetails)
+                    {
+                        tot += producto.SaleDetailsTotal;
+                    }
+                }
+
+                return tot;
+            }
+        }
+
+        public List<SaleDetailsViewModel> SaleDetails { get; set; }
+    }
+}
